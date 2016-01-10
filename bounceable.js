e529982e27c70d483c20c801237fc93b91fec7e2
Steve Dakh <slavik0329@gmail.com>
2016-01-09 20:16:36 -0500

    initial commit

---
+++ bounceable.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var React = require('react-native');
+
+const moveTolerance = 30;
+
+var {
+  Animated,
+  PanResponder,
+  View,
+} = React;
+
+module.exports = React.createClass({
+  getInitialState() {
+      return {
+        scale: new Animated.Value(1)
+      };
+  },
+  getDefaultProps() {
+      return {
+          level: 1.1  
+      };
+  },
+  _panResponder: {},
+  componentWillMount() {
+      this._panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (evt, gestureState) => true,
+        onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+        onMoveShouldSetPanResponder: (evt, gestureState) => true,
+        onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+        onPanResponderTerminationRequest: (evt, gestureState) => true,
+        onPanResponderTerminate: (evt, gestureState) => {
+        },
+        onPanResponderGrant: (evt, gestureState) => { 
+          Animated.timing(         
+            this.state.scale,    
+            {
+              toValue: this.props.level,
+              friction:1,
+              duration: 200
+            },           
+          ).start();  
+        },
+
+        onPanResponderRelease: (evt, gestureState) => {
+
+          if ( gestureState.dy > moveTolerance ||  gestureState.dy < (-moveTolerance) || gestureState.dx > moveTolerance ||  gestureState.dx < (-moveTolerance)) {
+          } else {
+            if ( this.props.onPress ) {
+              setTimeout( () => { // 50ms delay makes it more natural
+                this.props.onPress(); 
+              }, 50);
+            }
+          }
+
+          Animated.spring(         
+            this.state.scale,    
+            {
+              toValue: 1,
+              friction:1,
+              duration: 200
+            },           
+          ).start();  
+        },
+
+      });  
+  },
+  render: function() {
+    return (
+        <Animated.View
+          style={[{
+              transform: [
+                {
+                  scale: this.state.scale
+                }
+              ]
+            }, this.props.style
+          ]}>
+          <View
+            {...this._panResponder.panHandlers}>
+            <View>
+              {this.props.children}
+            </View>
+          </View>
+        </Animated.View>
+    );
+  }
+});
