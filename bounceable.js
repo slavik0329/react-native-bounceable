805003bfff980db4400a0b19806531620fa0cd33
Steve Dakh <slavik0329@gmail.com>
2016-11-13 10:59:06 -0500

    Converted to ES6 and using react-native vs react npm module for native components

--- bounceable.js
+++ bounceable.js
@@ -1,92 +1,94 @@
 'use strict';
 
-var React = require('react-native');
+import React, {Component} from "react";
+import {Animated, PanResponder, View} from "react-native";
 
 const moveTolerance = 30; // Amount of movement before it is no longer a press
 
-var {
-  Animated,
-  PanResponder,
-  View,
-} = React;
+class Bounceable extends Component {
+  static propTypes = {
+    onPress: React.PropTypes.func, //Optional function to be excecuted after succesful press
+    level: React.PropTypes.number // Maximum scale of animation
+  };
 
-module.exports = React.createClass({
-  propTypes: {
-      onPress: React.PropTypes.func, //Optional function to be excecuted after succesful press
-      level: React.PropTypes.number // Maximum scale of animation
-  },
-  getDefaultProps() {
-      return {
-          level: 1.1  
-      };
-  },
-  getInitialState() {
-      return {
-        scale: new Animated.Value(1)
-      };
-  },
-  _panResponder: {},
+  static defaultProps = {
+      level: 1.1
+  };
+
+  constructor (props) {
+    super( props);
+
+    this.state = {
+      scale: new Animated.Value(1)
+    };
+  }
+
+  panResponder = {};
+
   componentWillMount() {
-      this._panResponder = PanResponder.create({
-        onStartShouldSetPanResponder: (evt, gestureState) => true,
-        onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-        onMoveShouldSetPanResponder: (evt, gestureState) => true,
-        onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-        onPanResponderTerminationRequest: (evt, gestureState) => true,
-        onPanResponderTerminate: (evt, gestureState) => {
-        },
-        
-        onPanResponderGrant: (evt, gestureState) => { 
-          Animated.timing(         
-            this.state.scale,    
-            {
-              toValue: this.props.level,
-              friction:1,
-              duration: 200
-            },           
-          ).start();  
-        },
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderTerminate: (evt, gestureState) => {
+      },
 
-        onPanResponderRelease: (evt, gestureState) => {
-          if ( gestureState.dy > moveTolerance ||  gestureState.dy < (-moveTolerance) || gestureState.dx > moveTolerance ||  gestureState.dx < (-moveTolerance)) {
-            // Do nothing 
-          } else {
-            setTimeout( () => { // 50ms delay makes press response more natural
-              Animated.spring(         
-                this.state.scale,    
-                {
-                  toValue: 1,
-                  friction:1,
-                  duration: 200
-                },           
-              ).start(); 
+      onPanResponderGrant: (evt, gestureState) => {
+        Animated.timing(
+          this.state.scale,
+          {
+            toValue: this.props.level,
+            friction: 1,
+            duration: 200
+          },
+        ).start();
+      },
 
-              if ( this.props.onPress ) {
-                this.props.onPress(); 
-              }
-            }, 50);
-          } 
+      onPanResponderRelease: (evt, gestureState) => {
+        if (gestureState.dy > moveTolerance || gestureState.dy < (-moveTolerance) || gestureState.dx > moveTolerance || gestureState.dx < (-moveTolerance)) {
+          // Do nothing 
+        } else {
+          setTimeout(() => { // 50ms delay makes press response more natural
+            Animated.spring(
+              this.state.scale,
+              {
+                toValue: 1,
+                friction: 1,
+                duration: 200
+              },
+            ).start();
+
+            if (this.props.onPress) {
+              this.props.onPress();
+            }
+          }, 50);
         }
-      });  
-  },
-  render: function() {
+      }
+    });
+  }
+
+  render () {
     return (
-        <Animated.View
-          style={[{
-              transform: [
-                {
-                  scale: this.state.scale
-                }
-              ]
-            }, this.props.style
-          ]}>
-          <View
-            {...this._panResponder.panHandlers}>
-            <View>
-              {this.props.children}
-            </View>
+      <Animated.View
+        style={[{
+          transform: [
+            {
+              scale: this.state.scale
+            }
+          ]
+        }, this.props.style
+        ]}>
+        <View
+          {...this._panResponder.panHandlers}>
+          <View>
+            {this.props.children}
           </View>
-        </Animated.View>
+        </View>
+      </Animated.View>
     );
   }
-});
+}
+
+module.exports = Bounceable;
